7972a41810713ffae7886adadc6fc6ab83e38095
Salih T <mhdsalih9656@gmail.com>
2023-01-16 09:34:21 +0530

    constents completed

--- lib/constends/text_const.dart
+++ lib/constends/text_const.dart
@@ -5,11 +5,20 @@ class ConstText {
   static String gender2 = "Female";
   static String gender3 = "Transgenter";
   static String successMsg = "Success";
+  static String errorName = "Please Enter Your Name";
+  static String errorNumber = "Please Enter Your Mobile Number";
+  static String errorGender = "Please Enter Your Gender";
   static String errorMsg1 = "Something went wrong";
   static String errorMsg2 = "Please Complete Credentilas";
+  static String errorMail = "Please Enter Your Email";
+  static String errorPass = "Please Enter your password";
   static String errormsg3 = "This email is already registred";
   static String errorMsg4 = "check your email";
   static String errorMsg5 = "check your password";
   static String signUpMsg = "This Email not registred";
   static String loginMsg = "Please Login Here";
+  static String appName = "CHITTI APP";
+  static String goToRegister = "New to Here! Register?";
+  static String signUp = "Sign Up";
+  static String signIn = "Sign In";
 }
